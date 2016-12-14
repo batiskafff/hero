37efe5a9a79dcaa82d320c0a2139c087ee4bce31
Egor Anisimov <Egor_Anisimov@epam.com>
2016-12-14 10:39:01 -0500

    Hero manual last step

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,34 +1,46 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
-import { HttpModule }     from '@angular/http';
+import './rxjs-extensions';
 
-import { AppComponent }        from './app.component';
-import { DashboardComponent }  from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-import { AppRoutingModule }    from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,39 +1,64 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
 
-import { Router }   from '@angular/router';
-
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { Hero }                from './hero';
+import { HeroService }         from './hero.service';
 
 @Component({
   moduleId: module.id,
   selector: 'my-heroes',
-  providers: [HeroService],
   templateUrl: 'heroes.component.html',
   styleUrls: [ 'heroes.component.css' ]
 })
-
 export class HeroesComponent implements OnInit {
-    heroes: Hero[];
-    selectedHero: Hero;
+  heroes: Hero[];
+  selectedHero: Hero;
 
-    constructor (private heroServce: HeroService, private router: Router) {
-    }
+  constructor(
+    private heroService: HeroService,
+    private router: Router) { }
 
-    ngOnInit(): void {
-        this.getHeroes();
-    }
+  getHeroes(): void {
+    this.heroService
+        .getHeroes()
+        .then(heroes => this.heroes = heroes);
+  }
 
-    getHeroes(): void {
-        this.heroServce.getHeroes().then(heroes => this.heroes = heroes);
-    }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
 
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
+  }
 
-    gotoDetail(): void {
-      this.router.navigate(['/detail', this.selectedHero.id]);
-    }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
